9741aace8be69cdde67c32d7c293a28f30f6c6cd
Your Name <nakuldesigner77@gmail.com>
2024-08-12 17:01:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ const App = () => {
+  return (
+    <main id='main'>
+      <div className='box'>
+
+        
+      </div>
+
+    </main>
+  )
+}
+
+
+export default App
