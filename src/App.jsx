130e0c41be0db015fafea7dacfa8f543925b7465
Your Name <nakuldesigner77@gmail.com>
2024-08-13 00:08:49 +0530

    Gsap animation with usegsap hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useRef, useState } from 'react'
 
- const App = () => {
+const App = () => {
+
+  const [circle, setCircle] = useState(0)
+
+  const random = gsap.utils.random(-500, 500, 100)
+
+  // const gsapRef = useRef()
+
+  useGSAP(() => {
+
+    gsap.to(".circle", {
+      x: circle,
+      duration: 0.5,
+    })
+
+  }, [circle])
+
+
   return (
     <main id='main'>
-      <div className='box'>
-
-        
-      </div>
+      <button onClick={() => setCircle(random)}>Animate</button>
+      <div className="circle"></div>
 
     </main>
   )
